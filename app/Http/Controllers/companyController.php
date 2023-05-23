1f73372cd293a4a8fe807250ec2a204a6e41a489
david5871 <davidbarath08@gmail.com>
2023-05-24 01:16:51 +0200

    if the company not exist drop not found exception, surrounded with try catch blocks

--- app/Http/Controllers/companyController.php
+++ app/Http/Controllers/companyController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\company;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 
 class companyController extends Controller
 {
@@ -21,46 +22,70 @@ class companyController extends Controller
 
     public function showCompany(Company $company)
     {
-        return view("display.company", ["company" => $company]);
-    }   
+        try {
+            return view("display.company", ["company" => $company]);
+        } 
+        catch (ModelNotFoundException $e) {
+            abort(404);
+        }
+    }
 
     public function editCompany(Company $company)
     {
-        return view("display.edit", ["company" => $company]);
+        try {
+            return view("display.edit", ["company" => $company]);
+        } 
+        catch (ModelNotFoundException $e) {
+            abort(404);
+        }
     }
 
     public function updateCompany(Request $request, Company $company)
     {
-        $validator = Validator::make($request->all(), [
-            'name' => 'required|string|max:255',
-            'taxNumber' => 'required|max:50|min:5',
-            'phone' => 'required|min:10',
-            'email' => 'required|email',
-        ]);
-
-        if ($validator->fails()) {
-            return redirect()
-                ->back()
-                ->withErrors($validator)
-                ->withInput();
+        try{
+            $validator = Validator::make($request->all(), [
+                'name' => 'required|string|max:255',
+                'taxNumber' => 'required|max:50|min:5',
+                'phone' => 'required|min:10',
+                'email' => 'required|email',
+            ]);
+    
+            if ($validator->fails()) {
+                return redirect()
+                    ->back()
+                    ->withErrors($validator)
+                    ->withInput();
+            }
+    
+            $validatedData = $validator->validated();
+    
+            $company->name = $validatedData["name"];
+            $company->taxNumber = $validatedData["taxNumber"];
+            $company->phone = $validatedData["phone"];
+            $company->email = $validatedData["email"];
+            $company->save();
+    
+            return redirect()->route('list')->with('success', 'Product updated successfully!');
+        } 
+        catch (ModelNotFoundException $e){
+            return abort(404);
         }
-
-        $validatedData = $validator->validated();
-
-        $company->name = $validatedData["name"];
-        $company->taxNumber = $validatedData["taxNumber"];
-        $company->phone = $validatedData["phone"];
-        $company->email = $validatedData["email"];
-        $company->save();
-
-        return redirect()->route('list')->with('success', 'Product updated successfully!');
+        
+        
     }
 
     public function destroyCompany(Company $company)
     {
-        $company->delete();
+        try{
+            $company->delete();
 
-        return redirect()->route('list')->with('success', 'Product Deleted successfully!');
+            return redirect()->route('list')->with('success', 'Product Deleted successfully!');
+        }
+        catch (ModelNotFoundException $e){
+            return abort(404);
+        }
+        
+       
     }
 
 
