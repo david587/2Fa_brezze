da1c4dba189b360020927054faf6049dda2d9a24
david5871 <davidbarath08@gmail.com>
2023-05-24 00:27:39 +0200

    link what return the user to home page

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -16,9 +16,15 @@
   </div>
 </div>
 <nav class="navbar navbar-dark bg-dark">
-  <div class="container-fluid">
-      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarToggleExternalContent" aria-controls="navbarToggleExternalContent" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-      </button>
+  <div class="container-fluid d-flex justify-content-between">
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarToggleExternalContent" aria-controls="navbarToggleExternalContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="text-center">
+      <a class="text-white" href="{{ route('list') }}">Home</a>
+    </div>
+    <div></div> <!-- spacing -->
   </div>
 </nav>
+
+
