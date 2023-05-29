b6fc34ecbaa05447db61a650820d6b4ea81281df
david5871 <davidbarath08@gmail.com>
2023-05-29 23:59:27 +0200

    more frontend validations

--- resources/views/display/create.blade.php
+++ resources/views/display/create.blade.php
@@ -8,7 +8,7 @@
         @csrf
         <div class="mb-3">
             <label for="name" class="form-label">Company Name</label>
-            <input value="{{ old('name') }}" type="text" class="form-control" id="name" name="name" placeholder="Your company" required>
+            <input value="{{ old('name') }}" type="text" class="form-control" id="name" name="name" placeholder="Your company" required pattern="[A-Za-z0-9\s]+" title="Please enter a valid company name (only letters, numbers, and spaces are allowed)">
         </div>
         @error("name") 
         <div class="alert alert-danger mt-3 mb-3" role="alert">
@@ -18,7 +18,7 @@
 
         <div class="mb-3">
             <label for="taxNumber" class="form-label">Tax Number</label>
-            <input value="{{ old('taxNumber') }}" type="number" class="form-control" id="taxNumber" name="taxNumber" placeholder="989899871" required>
+            <input value="{{ old('taxNumber') }}" type="text" class="form-control" id="taxNumber" name="taxNumber" placeholder="989899871" required pattern="[0-9]{9}" title="Please enter a 9-digit tax number">
         </div>
         @error("taxNumber") 
         <div class="alert alert-danger mt-3 mb-3" role="alert">
@@ -28,7 +28,7 @@
 
         <div class="mb-3">
             <label for="phone" class="form-label">Phone</label>
-            <input value="{{ old('phone') }}" type="text" class="form-control" id="phone" name="phone" placeholder="06308345693" required>
+            <input value="{{ old('phone') }}" type="text" class="form-control" id="phone" name="phone" placeholder="06308345693" required pattern="[0-9()+-]{10,20}" title="Please enter a valid phone number (including +, (), and -)">
         </div>
         @error("phone") 
         <div class="alert alert-danger mt-3 mb-3" role="alert">
@@ -38,7 +38,7 @@
 
         <div class="mb-3">
             <label for="email" class="form-label">Email</label>
-            <input value="{{ old('email') }}" type="text" class="form-control" id="email" name="email" placeholder="example@gmail.com" >
+            <input value="{{ old('email') }}" type="email" class="form-control" id="email" name="email" placeholder="example@gmail.com" required>
         </div>
         @error("email") 
         <div class="alert alert-danger mt-3 mb-3" role="alert">
